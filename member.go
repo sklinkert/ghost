e625c4a3aebc22118ccbae815fe3cd296835b23d
Simon Klinkert <simon.klinkert@gmail.com>
2025-12-03 00:13:47 +0100

    add admin member endpoints with pagination support
    - Add AdminGetMember(memberId) to get single member by ID
    - Add AdminDeleteMember(memberId) to delete a member
    - Update AdminGetMembers() with pagination support (100 per page)
    - Add Member struct fields: Comped, LastSeenAt, UnsubscribeUrl, Tiers,
      EmailSuppression, Newsletters, Attribution
    - Add supporting types: Tier, Newsletter, EmailSuppression, Attribution
    - Add pagination metadata to Members struct

--- member.go
+++ member.go
@@ -3,11 +3,14 @@ package ghost
 import (
 	"encoding/json"
 	"fmt"
+	"io"
+	"net/http"
 	"time"
 )
 
 type Members struct {
-	Members []Member `json:"members"`
+	Members []Member   `json:"members"`
+	Meta    Pagination `json:"meta,omitempty"`
 }
 
 type NewMembers struct {
@@ -31,12 +34,19 @@ type Member struct {
 		CreatedAt time.Time `json:"created_at"`
 		UpdatedAt time.Time `json:"updated_at"`
 	} `json:"labels"`
-	Subscriptions    []Subscription `json:"subscriptions"`
-	AvatarImage      string         `json:"avatar_image"`
-	EmailCount       int            `json:"email_count"`
-	EmailOpenedCount int            `json:"email_opened_count"`
-	EmailOpenRate    float64        `json:"email_open_rate"`
-	Status           string         `json:"status"`
+	Subscriptions    []Subscription   `json:"subscriptions"`
+	AvatarImage      string           `json:"avatar_image"`
+	Comped           bool             `json:"comped"`
+	EmailCount       int              `json:"email_count"`
+	EmailOpenedCount int              `json:"email_opened_count"`
+	EmailOpenRate    *float64         `json:"email_open_rate"`
+	Status           string           `json:"status"`
+	LastSeenAt       *time.Time       `json:"last_seen_at"`
+	UnsubscribeUrl   string           `json:"unsubscribe_url"`
+	Tiers            []Tier           `json:"tiers"`
+	EmailSuppression EmailSuppression `json:"email_suppression"`
+	Newsletters      []Newsletter     `json:"newsletters"`
+	Attribution      *Attribution     `json:"attribution,omitempty"`
 }
 
 type NewMember struct {
@@ -68,15 +78,55 @@ type Subscription struct {
 	} `json:"price"`
 }
 
-func (g *Ghost) AdminGetMembers() (Members, error) {
-	var membersResponse Members
-	const ghostPostsURLSuffix = "%s/ghost/api/v3/admin/members/?key=%s&limit=all"
+type Tier struct {
+	Id   string `json:"id"`
+	Name string `json:"name"`
+}
 
-	var url = fmt.Sprintf(ghostPostsURLSuffix, g.url, g.adminAPIToken)
-	if err := g.getJson(url, &membersResponse); err != nil {
-		return membersResponse, err
+type Newsletter struct {
+	Id          string `json:"id"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Status      string `json:"status"`
+}
+
+type EmailSuppression struct {
+	Suppressed bool        `json:"suppressed"`
+	Info       interface{} `json:"info"`
+}
+
+type Attribution struct {
+	Id             interface{} `json:"id"`
+	Type           string      `json:"type"`
+	Url            string      `json:"url"`
+	Title          string      `json:"title"`
+	ReferrerSource string      `json:"referrer_source"`
+	ReferrerMedium interface{} `json:"referrer_medium"`
+	ReferrerUrl    interface{} `json:"referrer_url"`
+}
+
+func (g *Ghost) AdminGetMembers() (Members, error) {
+	const limit = 100
+	var allMembers Members
+	page := 1
+
+	for {
+		url := fmt.Sprintf("%s/ghost/api/v3/admin/members/?limit=%d&page=%d", g.url, limit, page)
+
+		var pageMembers Members
+		if err := g.getJson(url, &pageMembers); err != nil {
+			return allMembers, err
+		}
+
+		allMembers.Members = append(allMembers.Members, pageMembers.Members...)
+
+		if pageMembers.Meta.Pagination.Next == nil {
+			break
+		}
+		page = *pageMembers.Meta.Pagination.Next
 	}
-	return membersResponse, nil
+
+	return allMembers, nil
 }
 
 func (g *Ghost) AdminCreateMember(member NewMember) (Members, error) {
@@ -93,4 +143,44 @@ func (g *Ghost) AdminCreateMember(member NewMember) (Members, error) {
 		return members, err
 	}
 	return members, nil
+}
+
+func (g *Ghost) AdminGetMember(memberId string) (Members, error) {
+	var members Members
+	url := fmt.Sprintf("%s/ghost/api/v3/admin/members/%s/?include=tiers", g.url, memberId)
+
+	if err := g.getJson(url, &members); err != nil {
+		return members, err
+	}
+	return members, nil
+}
+
+func (g *Ghost) AdminDeleteMember(memberId string) error {
+	if err := g.checkAndRenewJWT(); err != nil {
+		return err
+	}
+
+	deleteURL := fmt.Sprintf("%s/ghost/api/v3/admin/members/%s/", g.url, memberId)
+
+	req, err := http.NewRequest(http.MethodDelete, deleteURL, nil)
+	if err != nil {
+		return err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", "Ghost "+g.jwtToken)
+	resp, err := g.client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer func(Body io.ReadCloser) {
+		_ = Body.Close()
+	}(resp.Body)
+
+	content, _ := io.ReadAll(resp.Body)
+	responseBody := string(content)
+	if resp.StatusCode != http.StatusNoContent {
+		return fmt.Errorf("unexpected status code %d: %s", resp.StatusCode, responseBody)
+	}
+	return nil
 }
